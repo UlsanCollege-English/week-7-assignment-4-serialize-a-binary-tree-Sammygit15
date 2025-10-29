3b6a21c3d112aa97fcfd817fb9a5ec554b7b9cf0
Sammy Lama <sammy@example.com>
2025-10-29 10:26:58 +0545

    serialize

--- src/serialize.py
+++ src/serialize.py
@@ -5,9 +5,30 @@ class Node:
         self.right = right
 
 def serialize(root):
-    # TODO: preorder with '#' for None, space-separated
-    pass
+    """Serialize binary tree into string using preorder traversal with '#' for None."""
+    def helper(node):
+        if node is None:
+            return ['#']
+        return [str(node.val)] + helper(node.left) + helper(node.right)
+    
+    return ' '.join(helper(root))
 
 def deserialize(s):
-    # TODO: rebuild from tokens
-    pass
+    """Deserialize string back to binary tree."""
+    tokens = iter(s.split())
+
+    def helper():
+        val = next(tokens)
+        if val == '#':
+            return None
+        # Convert to int if possible
+        try:
+            val_cast = int(val)
+        except ValueError:
+            val_cast = val
+        node = Node(val_cast)
+        node.left = helper()
+        node.right = helper()
+        return node
+
+    return helper()
